176dd63ca34e8d773f9e28e9cca0456f87e1b6b5
voldrek <lucasjordano91@gmail.com>
2024-10-17 22:24:49 -0300

    Primeiro commit Screen match do curso da alura utilizando Spring bot e java

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=201cb905");
+		System.out.println(json);
+	}
+}
